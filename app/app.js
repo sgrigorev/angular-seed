f2f89d86379009ad4fc0d556bf2bf9196b106a0b
Sergey Grigorev <grigorev-s.n@yandex.ru>
2018-12-12 00:17:29 +0300

    Changed routing

--- app/app.js
+++ app/app.js
@@ -7,8 +7,34 @@ angular.module('myApp', [
   'myApp.view2',
   'myApp.version'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+  config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('');
+    $locationProvider.html5Mode(true)
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    $routeProvider
+      .when('/view1', {
+        templateUrl: 'view1/view1.html',
+        controller: 'View1Ctrl',
+        resolve: {
+          // I will cause a 1 second delay
+          delay: function ($q, $timeout) {
+            var delay = $q.defer();
+            $timeout(delay.resolve, 1000);
+            return delay.promise;
+          }
+        }
+      })
+      .when('/view2', {
+        templateUrl: 'view2/view2.html',
+        controller: 'View2Ctrl',
+        resolve: {
+          // I will cause a 1 second delay
+          delay: function ($q, $timeout) {
+            var delay = $q.defer();
+            $timeout(delay.resolve, 1000);
+            return delay.promise;
+          }
+        }
+      })
+      .otherwise({ redirectTo: '/view1' });
+  }]);
